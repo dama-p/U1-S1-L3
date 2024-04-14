5092684d4efa3af2c6494ffb90f9c465ebe78b42
Damaride Putignano <damarideputignano@gmail.com>
2024-04-14 16:26:07 +0200

    Fixed editing issue

--- edit-logic.php
+++ edit-logic.php
@@ -5,13 +5,16 @@ $id = $_POST['id'];
 $name = $_POST['name'];
 $surname = $_POST['surname'];
 $age = $_POST['age'];
+$hobbies = $_POST['hobbies'];
 
-$stmt = $pdo->prepare("UPDATE Users SET Name = :name, Surname = :surname, Age = :age WHERE id = :id");
+
+$stmt = $pdo->prepare("UPDATE Users SET Name = :name, Surname = :surname, Age = :age, Hobbies = :hobbies WHERE id = :id");
 $stmt->execute([
     'id' => $id,
     'name' => $name,
     'surname' => $surname,
     'age' => $age,
+    'hobbies' => $hobbies,
 ]);
 
 header("Location: /FSD%20IFOA/U1-S1-L3/searchbar.php/");
